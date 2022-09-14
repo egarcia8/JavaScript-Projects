367926ea572bad4c992f509d10f71181362ad082
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-09-14 12:18:35 -0500

    Created function project
    Added basic example of a function

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+    var str = "Good morning!";
+    var str2 = "Have a great day!";
+    document.write(str2)
+    document.getElementById("Time_Button").innerHTML = str2;
+}
