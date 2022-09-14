034ff0fea49e532d22dbf7f49308ceb0bb323151
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-09-14 09:32:23 -0500

    Created first JavaScript project
    Added HTML file linking to a main.js file

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("Hello, world!"); //Utlized window alert to create a pop up window
+
+var Sent1 = "This is the beginning of the string"; //Defined a variable giving it a string value
+
+var Sent2 = " and this is the end of the string.";
+
+document.write(Sent1 + Sent2 + "<br>"); //concatenating Sent 1 and Sent2 variables together
+                                        //and adding a break at the end of the line
+var Family = "Garcia", Mom = "Evelina", Dad = "Dennis", Son = "Nicolas", Son2 = "Sebastian",
+    Son3 = "Elijah", Dog = "Charlie"; // Defining mutliple variables and gving them a string value
+document.write(Dog + "<br>"); 
+
+document.write(4 * 5); //Using an expression
