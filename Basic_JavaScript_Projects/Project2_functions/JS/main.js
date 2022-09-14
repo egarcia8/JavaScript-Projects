d007fcde6b6572e0b12e8d353086835e4bdd5f8a
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-09-14 12:31:18 -0500

    Added function
    added a function to concatenate using the += operator

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,6 +1,11 @@
-function My_First_Function() {
-    var str = "Good morning!";
+function My_First_Function() {  //This is a function that declares a variable and displays it when the button is clicked
     var str2 = "Have a great day!";
-    document.write(str2)
+    document.write(str2) //This allows the to be displayed as a string not a button
     document.getElementById("Time_Button").innerHTML = str2;
+}
+
+function myFunction() {  //a function that concateates using the += operator
+    var sent = "It is a beautiful morning";
+    sent += " when the sun is shining.";
+    document.getElementById("Concatenate").innerHTML = sent;
 }
