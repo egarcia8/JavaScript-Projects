3b25895abb57a7c2a73f8e6c9401374d65d83c1e
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-09-15 12:02:53 -0500

    Added function
    added and addition function

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,6 +1,5 @@
 function My_First_Function() {  //This is a function that declares a variable and displays it when the button is clicked
     var str2 = "Have a great day!";
-    document.write(str2) //This allows the to be displayed as a string not a button
     document.getElementById("Time_Button").innerHTML = str2;
 }
 
@@ -9,3 +8,8 @@ function myFunction() {  //a function that concateates using the += operator
     sent += " when the sun is shining.";
     document.getElementById("Concatenate").innerHTML = sent;
 }
+
+function addClasses(a, b) {
+    return a + b;    
+}
+document.getElementById("test").innerHTML = addClasses(23, 27);   
